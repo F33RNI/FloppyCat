7f6ea8f14681b27ae0be3f03d3388eb5869dcbff
F33RNI <xxoinvizionxx@gmail.com>
2024-01-22 13:23:20 -0500

    style: change header of _version.py and _control_values.py

--- _version.py
+++ _version.py
@@ -1,22 +1,19 @@
 """
- Copyright (C) 2023 Fern Lane, FloppyCat Simple Backup Utility
+This file is part of the FloppyCat Simple Backup Utility distribution
+(https://github.com/F33RNI/FloppyCat)
 
- Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Copyright (C) 2023-2024 Fern Lane
 
-       https://www.gnu.org/licenses/agpl-3.0.en.html
+This program is free software: you can redistribute it and/or modify it under the terms 
+of the GNU Affero General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
 
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR
- OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
+You should have received a copy of the GNU Affero General Public License long with this program.
+If not, see <http://www.gnu.org/licenses/>.
 """
 
 # FloppyCat Simple Backup Utility version
